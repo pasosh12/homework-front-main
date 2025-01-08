3f5e54ddbd1e706f3aa43459e06776431c0e7f20
Daniil <daniil.shishaev@gmail.com>
2025-01-08 16:54:50 +0300

    hw-3

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -10,11 +10,11 @@ export type UserType = {
   id: number;
   name: string;
   age: number;
-  address: any; // пропиши типизацию
+  address: AddressType;
 };
 
 export type UsersObjectType = {
-  myFriends: any; // пропиши типизацию
+  myFriends: UserType[];
 };
 
 export const HW2 = () => {
@@ -46,14 +46,17 @@ export const HW2 = () => {
 
   let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
 
-  const filterUsers = () => {
-    const filteredUsers = 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
+  const filterUsers  = ()  => {
+    const filteredUsers = currentUsers.myFriends.filter((filteringUser)=>{
+     return  filteringUser.address.city === 'Los Angeles'
+    })//'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
+      console.log(filteredUsers)
     setCurrentUsers({ myFriends: filteredUsers });
   };
 
   return (
     <div id={'hw02'}>
-      <UserList2 users={currentUsers} filterUsers={filterUsers} />
+      <UserList2 users={currentUsers} filterUsers = {filterUsers} />
     </div>
   );
 };
--- src/HW/2/UserList2.tsx
+++ src/HW/2/UserList2.tsx
@@ -2,22 +2,24 @@ import {CurrentUser} from './CurrentUser';
 import {UsersObjectType} from './HW2';
 
 type UserList2PropsType = {
-	users: any // пропиши типизацию
-	filterUsers: any // пропиши типизацию
+    users: UsersObjectType; // пропиши типизацию
+    filterUsers: ()=>void;
+    // filterUsers: Function;
 };
 
 export const UserList2 = (props: UserList2PropsType) => {
-	return (
-		<div id={'hw02-users'}>
-			<h2>User List:</h2>
 
-			{/*<button id={'hw02-filter-button'} onClick={() => props.filterUsers}>SHOW ME FRIENDS FROM LA</button>*/}
+    return (
+        <div id={'hw02-users'}>
+            <h2>User List:</h2>
 
-			{/*<ul>*/}
-			{/*  {props.users.myFriends.map((user) => (*/}
-			{/*    <ВЫЗЫВАЕМ КОМПОНЕНТУ CurrentUser />*/}
-			{/*  ))}*/}
-			{/*</ul>*/}
-		</div>
-	);
+            <button id={'hw02-filter-button'} onClick={props.filterUsers}>SHOW ME FRIENDS FROM LA</button>
+
+            <ul>
+                {props.users.myFriends.map((user) => (
+                    <CurrentUser key={user.id} user={user}/>
+                ))}
+            </ul>
+        </div>
+    );
 };
